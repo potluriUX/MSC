10e5bb2d5ba47496aa02d7ec63d351dd8a9b5ae1
ravitejapotluri <spoorthy.devineni@gmail.com>
2014-01-29 14:45:52 -0500

    cleaned sorting

--- src/com/rm/kismet_cooking/GetYouTubeHistoryVideosTask.java
+++ src/com/rm/kismet_cooking/GetYouTubeHistoryVideosTask.java
@@ -126,26 +126,6 @@ public class GetYouTubeHistoryVideosTask implements Runnable {
 			Log.e("Feck", e);
 		}
 	}
-	  private static Map sortByComparator(Map unsortMap)
-	   {
-	        List list=new LinkedList(unsortMap.entrySet());
-	        //sort list based on comparator
-	        Collections.sort(list,new Comparator()
-	        {
-	             public int compare(Object o1,Object o2)
-	             {
-	     return ((Comparable)((Map.Entry)(o2)).getKey()).compareTo(((Map.Entry) (o1)).getKey());
-	             }
-	        });
-
-	        //put sorted list into map again
-	    Map sortedMap=new LinkedHashMap();
-	    for (Iterator it=list.iterator();it.hasNext();)
-	    {
-	         Map.Entry entry=(Map.Entry)it.next();
-	         sortedMap.put(entry.getKey(),entry.getValue());
-	    }
-	    return sortedMap;
-	   }   
+   
 	
 }
